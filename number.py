271497a18b07988433205787495bc2a401014fc2
nidhi334 <knidhibharti8178@gmail.com>
2025-09-26 15:14:46 +0530

    first commit

---
+++ number.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):  # check divisors up to sqrt(n)
+        if n % i == 0:
+            return False
+    return True
+
+def prime_in_range(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+# Test
+start = int(input("Enter start of range: "))
+end = int(input("Enter end of range: "))
+
+print("Prime numbers:", prime_in_range(start, end))
